c64e9183a9734cfaa25988731e89c9a9cceb5f7f
Skulkers <steve.pasieka@gmail.com>
2016-04-04 00:00:37 -0400

    initial page

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 
-function onFirstLoad() {
-	// Start teh page doing cool stuff
-	alert(" I am being annoying")
-	console.log("This is the best way to make updates and debug")
-}
+$(function() {
+    $('#videochange').hover(function() {
+      $(this).attr('src','Skulk_The_Hulking_By_Hook_Or_By_Crooked_Automatic_Assault_Rifle.gif');
+    },function() {
+      $(this).attr('src','videostill1.gif');
+    }
+});
