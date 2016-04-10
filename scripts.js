e01ef87ce8a9c529e5655b0bdb92f80511cb8654
Kyle Polich <kyle@datascience.com>
2016-04-10 16:38:41 -0700

    some updates

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
 
 $(document).ready(function () {
-	$(#showbounce).mouseenter(function(){
-		$(this).effect('bounce', 500)
-	});
-	(function() {
+	$('#showbounce').mouseenter(function(){
+               $(this).effect('bounce', 500)
+       });
+
     $('#videochange').hover(function() {
       $(this).attr('src','Skulk_The_Hulking_By_Hook_Or_By_Crooked_Automatic_Assault_Rifle.gif');
     },function() {
       $(this).attr('src','videostill1.gif');
-    }
+    })
 });
-})
 
