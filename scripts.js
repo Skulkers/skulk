4dab5b2383975215f49b0ec8fec959fbb20b908c
Steve <steve.pasieka@gmail.com>
2016-04-10 23:47:16 -0400

    update

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 
 $(document).ready(function () {
 	$('#showbounce').mouseenter(function(){
-               $(this).effect('bounce', 500)
+               $(this).effect('bounce', 1000)
        });
 
     $('#videochange').hover(function() {
       $(this).attr('src','Skulk_The_Hulking_By_Hook_Or_By_Crooked_Automatic_Assault_Rifle.gif');
     },function() {
       $(this).attr('src','videostill1.gif');
-    })
+    });
+    $('#musicpulse').mouseenter(function(){
+               $(this).effect('bounce', 1000)
+       });
+    $('#biohighlight').hover(function() {
+      $(this).toggle("highlight");
+    },function() {
+      $(this).toggle("highlight");
+})
 });
 
